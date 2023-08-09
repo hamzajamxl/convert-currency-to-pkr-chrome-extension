3e5f8e63129b7add1e4d3a51336250c811289113
Hamza Jamil <lyallpurlad@gmail.com>
2023-08-10 02:39:50 +0500

    API Fetched and Integrated Successfuly

--- exchange.js
+++ exchange.js
@@ -1,28 +1,31 @@
-const amount = document.getElementById('amount');
-const currency = document.getElementById('currency');
-const convert = document.getElementById('convert');
-const result = document.getElementById('result');
+document.addEventListener('DOMContentLoaded', function() {
+    const amount = document.getElementById('amount');
+    const currency = document.getElementById('currency');
+    const convert = document.getElementById('convert');
+    const result = document.getElementById('result');
 
-const API_KEY=""
-const API_URL=""
+    const API_KEY=""
+    const API_URL="https://api.api-ninjas.com/v1/exchangerate?pair="
 
-convert.addEventListener('click', () => {
-    const amountTotal= amount.value;
-    const currencyTotal = currency.value;
-    const url = API_URL + currencyTotal;
+    convert.addEventListener('click', () => {
+        const amountTotal= amount.value;
+        const currencyTotal = currency.value;
+        const url = API_URL + currencyTotal + "_PKR";
 
     fetch(url, {
         headers: {
             'X-API-KEY': API_KEY
         }
     })
+    .then(response => response.json())
     .then(data => {
-        const rate = data.rate;
-        const result = amountTotal * rate;
-        result.innerHTML = `${amount} ${currency} = ${result.toFixed(2)} PKR`
+        const rate = data.exchange_rate;
+        const resultPrice = amountTotal * rate;
+        result.innerHTML = `${amountTotal} ${currencyTotal} = ${resultPrice.toFixed(2)} PKR`;
     })
     .catch(error => {
         console.error('Request Failed: ', error);
-        result.innerHTML = 'error occured please try again later';
+        resultPrice.innerHTML = 'error occured please try again later';
     })
+})
 })
