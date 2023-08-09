8e00f13e9824d4069f4bce1db6c745d7ffe79f59
Hamza Jamil <lyallpurlad@gmail.com>
2023-08-09 18:37:15 +0500

    Getting HTML Elements by ID

---
+++ exchange.js
@@ -0,0 +1,4 @@
+const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
+const convert = document.getElementById('convert');
+const result = document.getElementById('result');
